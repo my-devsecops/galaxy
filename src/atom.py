1f550c2a05873eed6faf7188933fa92531163f32
Ayman <amahmoud@paloaltonetworks.com>
2020-06-04 16:31:04 +0400

    Secrets in Cookies ..

--- src/atom.py
+++ src/atom.py
@@ -1,1 +1,15 @@
-print ("Initializing ..")
+from flask import Flask, make_response, request
+
+app = Flask("Leak password")
+
+
+def render_template(data):
+    return None
+
+
+@app.route('/')
+def index():
+    password = request.args.get("password")
+    resp = make_response(render_template(...))
+    resp.set_cookie("password", password)
+    return resp
